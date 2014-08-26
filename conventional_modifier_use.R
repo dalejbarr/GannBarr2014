a2a5dbe719d7952637b840eeab068cba74b5a062
Dale Barr <dalejbarr3@gmail.com>
2014-08-26 22:28:15 +0100

    new syntax for diagonal model

--- conventional_modifier_use.R
+++ conventional_modifier_use.R
@@ -18,48 +18,43 @@ crefs_mit2c <- transform(crefs_mit2,
                      Fc=F-mean(F))                          
 
 # warning: this will take a long time, but doesn't converge
-crefs_mit2c_m1 <- glmer(Modifier ~ Ac*Nc*Fc +
-                        (1 + Nc*Fc | SessionID) + (1 + Ac*Nc | ItemID),
-                        crefs_mit2c, family=binomial(link="logit"),
-                        control=glmerControl(optimizer="bobyqa"))
+m_max <- glmer(Modifier ~ Ac*Nc*Fc +
+               (1 + Nc*Fc | SessionID) + (1 + Ac*Nc | ItemID),
+               crefs_mit2c, family=binomial(link="logit"),
+               control=glmerControl(optimizer="bobyqa"))
 
-# no-random-correlations model
-crefs_mit2c_m2 <- glmer(Modifier ~ Ac*Nc*Fc + 
-                        (1 | SessionID) +
-                        (0 + Nc | SessionID) + (0 + Fc | SessionID) +
-                        (0 + Nc:Fc | SessionID) +
-                        (1 | ItemID) +
-                        (0 + Ac | ItemID) + (0 + Nc | ItemID) +
-                        (0 + Ac:Nc | ItemID),
-                        crefs_mit2c, family=binomial(link="logit"),
-                        control=glmerControl(optimizer="bobyqa")) # converges
+# "diagonal" model; estimate variances only, covariances fixed to zero
+m_diag <- glmer(Modifier ~ Ac*Nc*Fc +
+                (1 + Nc*Fc || SessionID) + (1 + Ac*Nc || ItemID),
+                crefs_mit2c, family=binomial(link="logit"),
+                control=glmerControl(optimizer="bobyqa"))
 
-crefs_mit2c_m2_noA <- update(crefs_mit2c_m2, . ~ . - Ac) 
-crefs_mit2c_m2_noN <- update(crefs_mit2c_m2, . ~ . - Nc) # didn't converge
-crefs_mit2c_m2_noF <- update(crefs_mit2c_m2, . ~ . - Fc) 
-crefs_mit2c_m2_noAN <- update(crefs_mit2c_m2, . ~ . - Ac:Nc)
-crefs_mit2c_m2_noAF <- update(crefs_mit2c_m2, . ~ . - Ac:Fc)
-crefs_mit2c_m2_noNF <- update(crefs_mit2c_m2, . ~ . - Nc:Fc)
-crefs_mit2c_m2_noANF <- update(crefs_mit2c_m2, . ~ . - Ac:Nc:Fc) 
+m_diag_noA <- update(m_diag, . ~ . - Ac) 
+m_diag_noN <- update(m_diag, . ~ . - Nc)
+m_diag_noF <- update(m_diag, . ~ . - Fc) 
+m_diag_noAN <- update(m_diag, . ~ . - Ac:Nc)
+m_diag_noAF <- update(m_diag, . ~ . - Ac:Fc)
+m_diag_noNF <- update(m_diag, . ~ . - Nc:Fc)
+m_diag_noANF <- update(m_diag, . ~ . - Ac:Nc:Fc) 
 
-anova(crefs_mit2c_m2, crefs_mit2c_m2_noA)
-anova(crefs_mit2c_m2, crefs_mit2c_m2_noN)
-anova(crefs_mit2c_m2, crefs_mit2c_m2_noF)
-anova(crefs_mit2c_m2, crefs_mit2c_m2_noAN)
-anova(crefs_mit2c_m2, crefs_mit2c_m2_noAF)
-anova(crefs_mit2c_m2, crefs_mit2c_m2_noNF)
-anova(crefs_mit2c_m2, crefs_mit2c_m2_noANF)
+anova(m_diag, m_diag_noA)
+anova(m_diag, m_diag_noN)
+anova(m_diag, m_diag_noF)
+anova(m_diag, m_diag_noAN)
+anova(m_diag, m_diag_noAF)
+anova(m_diag, m_diag_noNF)
+anova(m_diag, m_diag_noANF)
 
 # very low modifier rates for "New Referent" condition
 #
 # let's drop that condition, analyze only OldRef condition
 crefs_newonly <- subset(crefs_mit2c, Novelty=="Old")
 
-crefs_newonly_m1 <-
+newonly_max <-
     glmer(Modifier ~ A*F +
           (1 + F | SessionID) +
           (1 + A| ItemID),
           data=crefs_newonly, family=binomial(link="logit"),
           control=glmerControl(optimizer="bobyqa"))
 
-summary(crefs_newonly_m1)
+summary(newonly_max)
